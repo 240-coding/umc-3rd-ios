5718e02c17a486255f6b605b015a8567b9c61cc6
Seoyoung Lee <wow9176@naver.com>
2022-10-15 03:15:38 +0900

    [Feat] 메모 앱 전체 UI 구현

--- Week4/Week4/ViewController.swift
+++ Week4/Week4/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.title = "메모"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis.circle"), style: .plain, target: self, action: nil)
+        
+        tableView.layer.cornerRadius = 10
+        tableView.layer.masksToBounds = true
+        
+        if #available(iOS 15.0, *) {
+            tableView.tableHeaderView = UIView()
+        }
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MemoCell") as? MemoTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.titleLabel.text = "히히"
+        cell.dateLabel.text = "2022/10/01"
+        cell.contentLabel.text = "안녕하세요"
+        
+        if indexPath.row == tableView.numberOfRows(inSection: indexPath.section) - 1 {
+            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: tableView.bounds.width)
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
